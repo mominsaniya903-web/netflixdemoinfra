e134ed95df9c6b28cfa87515885d79965827021d
mominsaniya903-web <mominsaniya903@gmail.com>
2026-08-24 21:30:55 +0530

    Refactor instance names and enable S3 versioning
    Updated instance names format and added S3 bucket versioning.

--- main.tf
+++ main.tf
@@ -8,18 +8,32 @@ resource "aws_instance" "one" {
   instance_type          = "t2.medium"
   key_name               = "saniya"
   vpc_security_group_ids = ["sg-0c960426f2820c282"]
+
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "Monitoring server"]
+  default = [
+    "jenkins",
+    "tomcat-1",
+    "Monitoring-server"
+  ]
 }
 
 resource "aws_s3_bucket" "one" {
   bucket = "my-project-bucket-123456789"
-  versioning {
-    enabled = true
+
+  tags = {
+    Name = "my-project-bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "one" {
+  bucket = aws_s3_bucket.one.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
