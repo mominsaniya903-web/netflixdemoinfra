fef6bdc89aa17f1d7628491312f24968181d657a
mominsaniya903-web <mominsaniya903@gmail.com>
2026-08-24 21:09:00 +0530

    Change key_name from 'junks' to 'saniya'

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 3
   ami                    = "ami-0b6d9d3d33ba97d99"
   instance_type          = "t2.medium"
-  key_name               = "junks"
+  key_name               = "saniya"
   vpc_security_group_ids = ["sg-0c960426f2820c282"]
   tags = {
     Name = var.instance_names[count.index]
