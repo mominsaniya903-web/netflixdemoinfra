aadf589b1fd51f25de76aa96505cc87963028614
mominsaniya903-web <mominsaniya903@gmail.com>
2026-08-24 20:50:53 +0530

    Update AMI and security group IDs for AWS instance

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 3
-  ami                    = "ami-091b599f5f318ddd2"
+  ami                    = "ami-0b6d9d3d33ba97d99"
   instance_type          = "c7i-flex.large"
   key_name               = "junks"
-  vpc_security_group_ids = ["sg-0e8005380bc78269f"]
+  vpc_security_group_ids = ["sg-0c960426f2820c282"]
   tags = {
     Name = var.instance_names[count.index]
   }
